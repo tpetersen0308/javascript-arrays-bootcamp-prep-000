ff3285cdb31fbd31906f691ce4449d6b59bf2c8d
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 18:31:02 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  "bread",
+  "mild cheese",
+  "sharp cheese",
+  "butter",
+  "tomato",
+  "garlic",
+]
+
+var tomatoSauceIngredients = [
+  "tomato",
+  "garlic",
+  "olive oil",
+  "basil",
+  "oregano"
+]
+
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array = [element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
