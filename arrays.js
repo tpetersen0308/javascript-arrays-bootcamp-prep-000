78a900693ba65e77c3182978e92462514566184f
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 19:54:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -66,5 +66,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(-1)
+  return array.slice(0, array.length - 1)
 }
