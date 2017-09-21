b024db196db2d7653415ec343986b1bace93c5ab
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 19:38:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
